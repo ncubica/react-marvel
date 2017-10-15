fc7820a09f300294083705beabfa33a4ef76643e
YScott Domes <scott@musefind.com>
2017-10-15 14:39:14 -0700

    Got action dispatch working

--- src/components/hero/hero_results.js
+++ src/components/hero/hero_results.js
@@ -1,52 +1,61 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import { bindActionCreators} from 'redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import player_add_action from '../../actions/players/player_add_action';
+import show_alert_action from '../../actions/alert/show_alert_action';
 
 import HeroItem from './hero_results_item';
 
 class HeroDetail extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-
-    constructor(props) {
-        super(props);
+  onClickHandler(hero) {
+    if (hero) {
+      this.props.add_player(hero);
+      this.props.show_alert('Hello')
+      alert(
+        "We added a super hero, go and see the 'my heroes' tab. \n\n - sorry for this horrible alert, need more UX/UI :P -"
+      );
     }
+  }
 
-    onClickHandler(hero) {
-        if(hero) {
-            this.props.add_player(hero);
-            alert("We added a super hero, go and see the 'my heroes' tab. \n\n - sorry for this horrible alert, need more UX/UI :P -");
-        }
+  renderResults() {
+    if (this.props.currentHero) {
+      let heroes = this.props.currentHero;
+      return heroes.map(hero => {
+        return (
+          <div className="card" key={hero.id}>
+            <HeroItem
+              name={hero.name}
+              image={`${hero.thumbnail.path}.${hero.thumbnail.extension}`}
+            />
+            <button onClick={this.onClickHandler.bind(this, hero)}>
+              <i className="fa fa-plus" aria-hidden="true" />
+            </button>
+          </div>
+        );
+      });
     }
+  }
 
-    renderResults() {
-        if(this.props.currentHero) {
-            let heroes = this.props.currentHero;
-            return heroes.map((hero)=> {
-                return (
-                    <div className="card" key={hero.id}>
-                        <HeroItem name={hero.name} image={`${hero.thumbnail.path}.${hero.thumbnail.extension}`} />
-                        <button onClick={this.onClickHandler.bind(this, hero)}><i className="fa fa-plus" aria-hidden="true"></i></button>
-                    </div>
-                );
-            });
-        }
-    }
-
-    render() {
-        return (<div>{ this.renderResults() }</div>);
-    }
+  render() {
+    return <div>{this.renderResults()}</div>;
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-        currentHero : state.currentHero
-    }
+  return {
+    currentHero: state.currentHero,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators({add_player : player_add_action}, dispatch);
+  return bindActionCreators(
+    { add_player: player_add_action, show_alert: show_alert_action },
+    dispatch
+  );
 }
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(HeroDetail);
--- src/reducers/reducer_alert.js
+++ src/reducers/reducer_alert.js
@@ -1,4 +1,6 @@
-import { ACTION_SHOW_ALERT, ACTION_HIDE_ALERT } from '../actions';
+import {ACTION_SHOW_ALERT} from '../actions/alert/show_alert_action';
+import {ACTION_HIDE_ALERT} from '../actions/alert/hide_alert_action';
+
 export default function(state = null, action) {
   switch (action.type) {
     case ACTION_SHOW_ALERT:
